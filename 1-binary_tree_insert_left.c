d1d5cd949b5eaeb6a4a9e0e95f16aa70847861e4
KVAcodes <kadiriavictor@gmail.com>
2023-08-08 14:59:04 +0100

    fixed linter errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,17 +5,17 @@
  *							 another node.
  * @parent: pointer to the node to insert the left-child in.
  * @value: value to store in the new node.
- * 
+ *
  * Return: a pointer the created node or NULL on failure or if
  *		   parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 		return (NULL);
-	
+
 	new_node = binary_tree_node(parent, value);
 
 	if (parent->left)
@@ -25,9 +25,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 	}
 	else
-	{	
+	{
 		parent->left = new_node;
-		
 	}
 		return (new_node);
 }

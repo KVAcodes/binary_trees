d2cb94779c8dcdb4d9ab318d47f80410d088f7ab
KVAcodes <kadiriavictor@gmail.com>
2023-08-09 14:12:11 +0100

    making fixes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -45,7 +45,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (0);
 
@@ -65,6 +65,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_height(tree->left) -
-			binary_tree_height(tree->right));
+	return ((binary_tree_is_leaf(tree->left) ? 1 : 
+			binary_tree_height(tree->left)) -
+			(binary_tree_is_leaf(tree->right) ? 1 :
+			binary_tree_height(tree->right)));
 }

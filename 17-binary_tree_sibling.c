f62cf5b270edd08e009e1e019483942a5e2d1dff
KVAcodes <kadiriavictor@gmail.com>
2023-08-09 16:24:26 +0100

    fixed NULL omission

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,11 +10,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
-	if (!parent || !node)
+	if (!node || !node->parent)
 		return (NULL);
 
+	parent = node->parent;
 	if (parent->left == node && parent->right)
 		return (parent->right);
 	else if (parent->right == node && parent->left)

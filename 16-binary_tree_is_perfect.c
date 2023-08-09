1752fb989c80b284fde597a1cba2404df7ea2190
KVAcodes <kadiriavictor@gmail.com>
2023-08-09 16:03:25 +0100

    made some adjustments

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,39 +2,6 @@
 #include <math.h>
 
 /**
- * max - returns the greater of two values.
- *
- * @val_1: the first value.
- * @val_2: the second value.
- *
- * Return: the greater of the two values.
- */
-size_t max(size_t val_1, size_t val_2)
-{
-	if (val_1 > val_2)
-		return (val_1);
-	else
-		return (val_2);
-}
-
-/**
- * binary_tree_is_leaf- checks if a node is a leaf.
- *
- * @node: pointer to the node to check.
- *
- * Return: 1 if node is leaf, otherwise 0.
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-
-	if (node->left  == NULL && node->right == NULL)
-		return (1);
-
-	return (0);
-}
-/**
  * binary_tree_height - measures the height of a binary tree.
  *
  * @tree: pointer to the root node of the tree to measure the
@@ -44,11 +11,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || binary_tree_is_leaf(tree))
-		return (0);
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	return (max(binary_tree_height(tree->left),
-			binary_tree_height(tree->right)) + 1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
 /**
@@ -68,6 +39,26 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
+ * power - basic power function.
+ *
+ * @base: value
+ * @index: power
+ *
+ * Return: the base to the power of index
+ */
+int power(int base, int index)
+{
+	int tmp = base;
+
+	while (index != 0)
+	{
+		base *= tmp;
+		index--;
+	}
+	return (base);
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect.
  *
  * @tree: pointer to the root node of the tree to check.
@@ -81,7 +72,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	height = binary_tree_height(tree);
-	size_if_perfect = (int) pow(2, (double) height) + 1;
+	size_if_perfect = power(2, (int) height) - 1;
 
 	return ((int) binary_tree_size(tree) == size_if_perfect ?
 			1 : 0);
